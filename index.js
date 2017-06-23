d85fb321af6173f224986fcf3ac6792ffda58dc4
Benaka Moorthi <benaka.moorthi@mheducation.com>
2017-06-22 17:04:14 -0700

    Add prepublish hook + babel build for npm publish.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-export * from './src';
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+if (isDir(path.join(__dirname, 'dist'))) {
+  module.exports = require('./dist');
+} else {
+  module.exports = require('./src');
+}
+
+function isDir(pathToDir) {
+  try {
+    return fs.statSync(pathToDir).isDirectory();
+  } catch (e) {
+    return false;
+  }
+}
